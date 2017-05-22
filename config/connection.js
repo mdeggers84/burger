874db7740013e27b4d8ae2ef915e826039daa10a
Michael Eggers <mdeggers84@gmail.com>
2017-05-22 17:44:07 -0400

    troubleshooting connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,17 @@
 var mysql = require('mysql');
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-//   console.log(process.argv.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'burgers_db'
-//   });
-// }
-
-console.log(process.argv.JAWSDB_URL);
-connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log(process.argv.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
