13f09e9fbe24f5cdca308a506e3963d76b0d7bf3
Michael Eggers <mdeggers84@gmail.com>
2017-05-23 13:48:19 -0400

    added fun :smiley_cat:

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
+  var count = sessionStorage.getItem('count');
+
+  if (count === '3') {
+    $('#burger-img').hide();
+    $('#sloth-img').show();
+  } else {
+    $('#burger-img').show();
+    $('#sloth-img').hide();
+  }
+
   $('#add-btn').on('click', function () {
     if ($('#burger-name').val().trim() === '') {
       alert('Please insert a value');
     }
+  });
+
+  $('#clear-btn').on('click', function () {
+    sessionStorage.setItem('count', 0);
+  });
+
+  $(document).on('click', '.devour-btn', function () {
+    if (count !== null) {
+      count++;
+    } else {
+      count = 1;
+    }
+    sessionStorage.setItem('count', count);
   });
 });
